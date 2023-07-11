6182c1f294e63392f3ae5a322915608fb24ea0cc
sapientlion <calmwildcat98@protonmail.com>
2023-07-11 22:04:35 +0200

    Prepare project for its initial release.

--- legacy/source/main.c
+++ legacy/source/main.c
@@ -1,6 +1,24 @@
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//                                                                                                                   // 
+//    Outor - the ultimate outfit advisor and temperature converter made for fun.                                    //
+//    Copyright (C) 2023  Leo "SapientLion" Markoff                                                                  // 
+//                                                                                                                   //   
+//    This program is free software: you can redistribute it and/or modify                                           //
+//    it under the terms of the GNU General Public License as published by                                           //
+//    the Free Software Foundation, either version 3 of the License, or                                              //
+//    (at your option) any later version.                                                                            //   
+//                                                                                                                   //
+//    This program is distributed in the hope that it will be useful,                                                //
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of                                                 //
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the                                                  //
+//    GNU General Public License for more details.                                                                   //
+//                                                                                                                   //   
+//    You should have received a copy of the GNU General Public License                                              //
+//    along with this program.  If not, see <https://www.gnu.org/licenses/>.                                         //
+//
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 //																													 //
-// Description: Outfit Advisor: program driver definition.														     //
+//    Description: Outfit Advisor: program driver definition.														     //
 //																													 //
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
